4851d426bc70640af4beb5c877e8f3bdbe0fc3fe
Javvaji Nithin <57515502+Jnithin@users.noreply.github.com>
2020-12-18 00:46:12 +0530

    Update tree.js

--- tree.js
+++ tree.js
@@ -1,6 +1,5 @@
 /* Created by John NITHIN */
 
 name = prompt("Hello, What is your name?")
-alert("May this CHRISTMAS brings joy and happiness in your life, " + name +
-    ". Stay safe. JESUS be with you AMEN  (From JOHN NITHIN)")
-alert("HAPPY CHIRSTMAS TO YOU AND YOUR FAMILY")
+alert("MAY THIS CHRISTMAS BRINGS JOY AND HAPPINESS IN YOUR LIFE. " + name +
+    ".")
